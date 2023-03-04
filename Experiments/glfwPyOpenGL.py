53dee5e3d557c1bcfa496a2149e9bbb6a25b4a6c
Steve Pieper <pieper@isomics.com>
2023-03-04 14:32:10 -0500

    Try QWindow::fromWinId

--- Experiments/glfwPyOpenGL.py
+++ Experiments/glfwPyOpenGL.py
@@ -1,13 +1,18 @@
 """
+filePath = "/mnt/extra/pieper/SlicerWGPU/Experiments/glfwPyOpenGL.py"
 filePath = "/Users/pieper/slicer/latest/SlicerWGPU/Experiments/glfwPyOpenGL.py"
 
 exec(open(filePath).read())
 """
+
+import sys
+
+
 try:
     import glfw
     import OpenGL
     import OpenGL.GL
-except ModuleNotFoundException:
+except ModuleNotFoundError:
     pip_install("glfw")
     pip_install("PyOpenGL")
     import glfw
@@ -16,9 +21,30 @@ except ModuleNotFoundException:
 
 glfw.init()
 
-w = glfw.create_window(500,500,"hoot", None, None)
+glfwWindow = glfw.create_window(500,500,"hoot", None, None)
 
-glfw.make_context_current(w)
-OpenGL.GL.glClearColor(1., .5, .2, 1.)
-OpenGL.GL.glClear(OpenGL.GL.GL_COLOR_BUFFER_BIT)
-glfw.swap_buffers(w)
+def render(window):
+    glfw.make_context_current(window)
+    OpenGL.GL.glClearColor(1., .5, .2, 1.)
+    OpenGL.GL.glClear(OpenGL.GL.GL_COLOR_BUFFER_BIT)
+    glfw.swap_buffers(window)
+
+widget = qt.QWidget()
+layout = qt.QVBoxLayout(widget)
+
+if sys.platform == 'linux2':
+    qwindow = qt.QWindow.fromWinId(glfw.get_x11_window(glfwWindow))
+elif sys.platform == 'darwin':
+    qwindow = qt.QWindow.fromWinId(glfw.get_cocoa_window(glfwWindow))
+
+qcontainer = qt.QWidget.createWindowContainer(qwindow)
+layout.addWidget(qcontainer)
+
+
+renderButton = qt.QPushButton("Render")
+renderButton.connect("clicked()", lambda window=glfwWindow : render(window))
+
+layout.addWidget(renderButton)
+
+widget.show()
+
