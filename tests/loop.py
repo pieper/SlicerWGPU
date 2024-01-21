191b73cc6202b37b448083b57d9f819b706cdb4a
Steve Pieper <pieper@isomics.com>
2024-01-21 16:43:10 -0500

    Experiments to reoganize loop test
    Still fails even with different logic

--- tests/loop.py
+++ tests/loop.py
@@ -44,7 +44,7 @@ volumeIntArray = volumeArray.astype('int32')
 displacementsArray = numpy.zeros(shape=[3,3,3,4], dtype="float32")
 
 # wgsl Shader code
-shader = """
+shader_broken = """
 
 @group(0) @binding(0)
 var<storage,read> density: array<i32>;
@@ -70,6 +70,46 @@ fn main(@builtin(global_invocation_id) id: vec3<u32>) {
                       && ((idi32.x + ii) >= 0 && (idi32.x + ii) < dimensions.x) ) {
                     neighborsVisited += 1;
                 }
+            }
+        }
+    }
+    displacements[idi32.x + idi32.y * 3 + idi32.z * 9] = vec4<f32>(f32(kk), f32(jj), f32(ii), f32(neighborsVisited));
+}
+"""
+
+shader = """
+
+@group(0) @binding(0)
+var<storage,read> density: array<i32>;
+
+@group(0) @binding(1)
+var<storage,read_write> displacements: array<vec4<f32>>;
+
+@compute
+@workgroup_size(1)
+
+fn main(@builtin(global_invocation_id) id: vec3<u32>) {
+
+    let idi32 : vec3<i32> = vec3<i32>(id);
+    var dimensions : vec3<i32> = vec3<i32>(3,3,3);
+    var kk : i32; var jj : i32; var ii : i32;
+    var neighborsVisited : i32 = 0;
+    for (kk = -1; kk < 2; kk += 1) {
+        for (jj = -1; jj < 2; jj += 1) {
+            for (ii = -1; ii < 2; ii += 1) {
+                if (kk == 0 && jj == 0 && ii == 0) {
+                    break;
+                }
+                if (idi32.z + kk < 0 || idi32.z + kk > dimensions.z - 1) {
+                    break;
+                }
+                if (idi32.y + jj < 0 || idi32.y + jj > dimensions.y - 1) {
+                    break;
+                }
+                if (idi32.x + ii < 0 || idi32.x + ii > dimensions.x - 1) {
+                    break;
+                }
+                neighborsVisited += 1;
             }
         }
     }
@@ -139,7 +179,7 @@ displacementsArray = numpy.array(displacementsMemory.cast("f", displacementsArra
 print(displacementsArray[1,1,1])
 
 if displacementsArray[0,0,0][2] != 2:
-    print("Error: Inner loop variable not incrementing")
+    print(f"Error: Inner loop variable not incrementing: {displacementsArray[0,0,0]}")
 
 if displacementsArray.max() != 26:
-    print("Error: Inner loop variable not incrementing")
+    print(f"Error: incorrect neighbor count {displacementsArray.max()} != 26")
